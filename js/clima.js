03cfa3cda1a4e5c1d8414990252c3a0b16f3fa90
TARCISIO NUNES <tarcnux@gmail.com>
2019-11-22 21:34:29 -0300

    alterado para HTTPS

--- js/clima.js
+++ js/clima.js
@@ -1,7 +1,8 @@
 function getClima(){
     $.ajax({
-        method: 'get', crossDomain: true, url: 'http://api.openweathermap.org/data/2.5/weather?id=3468879&appid=971c8189ee4c574353095502279a73ff',
-        dataType: 'json', success: function(data){
+        method: 'get', crossDomain: true, url: 'https://api.openweathermap.org/data/2.5/weather?id=3468879&appid=971c8189ee4c574353095502279a73ff',
+        dataType: 'json', 
+        success: function(data){
             
             descricao = traduzirDescricao(data.weather[0].description);
             $('#situacao').html(descricao);
