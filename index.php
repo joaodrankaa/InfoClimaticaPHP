3d51871c726b30ac6e67da9a8b63701be5df00e3
Aluno <Aluno@sn632>
2019-11-29 23:44:37 -0200

    aa

--- index.php
+++ index.php
@@ -1,13 +1,54 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Projeto Climar</title>
+        <title>Projeto Clima</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="js/clima.js"></script>
+
+        <!-- Tags Gerais -->
+        <meta NAME="DESCRIPTION" CONTENT="site de informaçoes climaticas de brusque e região.Desenvolvido por João"/>
+        <meta NAME="ABSTRACT" CONTENT="informações climaticas"/>
+        <meta name="keywords" content="clima,temperatura de brusque,tempo de brusque,previsao de brusque,brusque,nascer do sol brusque,umidade de brusque,vai chover em brusque,dranka,chuva em brusque,sol em brusque,tempo brusque,vai da sol em brusque,tempo de brusque final de semana,final de semana,clima desenvolvido por dranka,região"/>
+        <meta NAME="title" CONTENT="Projeto Clima" />
+        <meta NAME="identifier-url" content="https://joao-clima-senai.herokuapp.com/" />
+        <meta NAME="author" content="João dranka" />
+        <meta NAME="ROBOTS" CONTENT="All" />
+        <meta NAME="RATING" CONTENT="general" />
+        <meta NAME="DISTRIBUTION" CONTENT="global" />
+        <meta NAME="LANGUAGE" CONTENT="pt-br" />
+        <meta name="content-language" content="portuguese" />
+        <meta name="doc-class" content="Completed" /> 
+        <meta name="reply-to" content="joao_v_dranka@estudante.sc.senai.br"/>
+
+
+
+
+        <!-- Tags Para rede social Facebook -->
+        <meta property="og:url" content="https://joao-clima-senai.herokuapp.com/" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Projeto Clima" />
+        <meta property="og:title" content="Projeto Clima" />
+        <meta property="og:description" content="site de informaçoes climaticas de brusque e região.Desenvolvido por João" />
+        <meta property="og:image" content="https://joao-clima-senai.herokuapp.com/img/icones/tMin.png" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="120" />
+        <meta property="fb:app_id" content="" />
+
+
+
+        <!-- Tags Para rede social Twitter -->
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:title" content="" />
+
+
+
+
     </head>
     <body>
         <div class="alert alert-danger mt-3">
@@ -63,8 +104,8 @@
                         <span id="situacao"></span>
                         <img src="img/icones/por-do-sol.png" width="100px">
                 </div>
-                
-                
+
+
                 <div class="col-md-6">
                     <h2>Anoitecer| <span id="Anoitecer"></span></h2>
                     <p>
